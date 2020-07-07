318ac7716af1c507fc55a058263a0826ad94f875
brandaoplaster <brandaoplaster@gmail.com>
2020-07-07 12:53:15 -0300

    Styling header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,12 @@ import SignIn from '../pages/SignIn';
 const Auth = createStackNavigator();
 
 const AuthRoutes: React.FC = () => (
-  <Auth.Navigator>
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#312e38' },
+    }}
+  >
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
   </Auth.Navigator>
