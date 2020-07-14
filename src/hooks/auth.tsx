91f91e3e3eff7770e862ddb689c92c86b7c7c45f
brandaoplaster <brandaoplaster@gmail.com>
2020-07-14 13:19:08 -0300

    Adding hooks auth

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,83 @@
+import React, {
+  createContext,
+  useCallback,
+  useState,
+  useContext,
+  useEffect,
+} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../service/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContext {
+  user: object;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const Auth = createContext<AuthContext>({} as AuthContext);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>({} as AuthState);
+
+  useEffect(() => {
+    async function loadStorageData(): Promise<void> {
+      const [token, user] = await AsyncStorage.multiGet([
+        '@Gobarber:token',
+        '@Gobarber:user',
+      ]);
+
+      if (token[1] && user[1]) {
+        setData({ token: token[1], user: JSON.parse(user[1]) });
+      }
+    }
+
+    loadStorageData();
+  }, []);
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+    await AsyncStorage.multiSet([
+      ['@Gobarber:token', token],
+      ['@Gobarber:user', JSON.stringify(user)],
+    ]);
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(['@Gobarber:token', '@Gobarber:user']);
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <Auth.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </Auth.Provider>
+  );
+};
+
+function useAuth(): AuthContext {
+  const context = useContext(Auth);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}
+
+export { AuthProvider, useAuth };
